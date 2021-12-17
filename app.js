892f4d08cba8b97c7535a57dd6400522d09ac48b
Charles DeBrosse <cmdebrosse@outlook.com?>
2021-12-17 14:23:05 -0500

    HTML template filled out

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -58,13 +60,14 @@ const promptProject = portfolioData => {
   if (!portfolioData.projects) {
     portfolioData.projects = [];
   };
-  return inquirer.prompt([
+  return inquirer
+  .prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is the name of your project? (Required)',
-      validate: projectNameInput => {
-        if (projectNameInput) {
+      validate: nameInput => {
+        if (nameInput) {
           return true;
         } else {
           console.log('Please give your project a name!');
@@ -76,8 +79,8 @@ const promptProject = portfolioData => {
       type: 'input',
       name: 'description',
       message: 'Provide a description of the project (Required)',
-      validate: projectDescription => {
-        if (projectDescription) {
+      validate: descriptionInput => {
+        if (descriptionInput) {
           return true;
         } else {
           console.log('Please describe your project!');
@@ -95,8 +98,8 @@ const promptProject = portfolioData => {
       type: 'input',
       name: 'link',
       message: 'Enter the GitHub link to your project. (Required)',
-      validate: projectLink => {
-        if (projectLink) {
+      validate: linkInput => {
+        if (linkInput) {
           return true;
         } else {
           console.log('Please provide a link to your project!');
@@ -130,16 +133,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html to see the output');
+    });
   });
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github)
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output');
-// });
